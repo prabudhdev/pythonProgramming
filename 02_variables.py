dbb781c4488e64640071594eed8dd01ec8819d30
Prabudh tyagi <prabudhtyagi@gmail.com>
2026-07-11 19:41:05 +0530

    learn variables

---
+++ 02_variables.py
@@ -0,0 +1,6 @@
+a = 2 # variable is a container where value is store
+b = 4
+
+name = "python"
+
+print(a+b) #here print the sum of a and b.
---
+++ 03_dataType.py
@@ -0,0 +1,10 @@
+a = 3   #  a is a integer.
+
+b = "python"  # b is a string
+
+c = 7.88  # c is a floting point no.
+
+d = True  # d is a boolean .
+
+e = None  # e is a none that's means nothing in this.
+  
