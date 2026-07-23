ee26bd2955a837e6c257ca26a7df9894322ba9af
Prabudh tyagi <prabudhtyagi@gmail.com>
2026-07-23 11:56:27 +0530

    learning Python

---
+++ operators.py/arthmaticOperator.py
@@ -0,0 +1,32 @@
+# + , - , / ,* 
+
+#   Here assigned values are operands.
+
+
+# addition operator'
+
+a = 4  
+b = 6
+
+print(a + b)
+
+# substraction operator 
+
+a = 4 
+b = 6
+
+print(a - b)
+
+# multiplication operator
+
+a = 4 
+b = 6
+
+print(a * b)
+
+# devision operator  
+
+a = 4 
+b = 6
+
+print(a / b)
---
+++ operators.py/assignedValues.py
@@ -0,0 +1,30 @@
+# assignment operator 
+
+#  =    this assign the value 
+ 
+a = 4
+print(a)
+# +=  this add the value and assign 
+
+b = 4
+b += 6
+print(b)
+
+# -= this subtract and assigned the value 
+
+c = 4
+c-= 8
+print(c)
+
+
+#  /=  this is difference and assigned the values
+
+d= 5
+d /= 5
+print(d)
+
+# *= This is multiply and assigned the values
+
+e = 4
+e *= 6
+print(e)
