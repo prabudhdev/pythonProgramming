1a373bdf514091aaaf6747cdba12ee5b0c67de2f
Prabudh tyagi <prabudhtyagi@gmail.com>
2026-07-30 23:33:32 +0530

    learning

--- 01_first.py
+++ 01_first.py
@@ -1,6 +1,8 @@
-print("Hello,World!")
+"""print("Hello,World!")
 
-#print(Python) It gives error because Python is not defined as a variable or string.
+#print(Python) 
+
+# It gives error because Python is not defined as a variable or string.
 
 print(25) 
 
@@ -18,5 +20,11 @@ print(5%2)
 
 print((5 + 5)*2)
 
+print("python" " " * 10)  #It prints the word python followed by 10 spaces.t
+"""
 
-  
+print("python's library")
+
+print('python\'s "library"')
+
+print("Path: C:\\new_folder\\notes\\\"day1\".txt")
