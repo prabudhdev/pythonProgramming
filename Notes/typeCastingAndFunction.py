459e71e563764881797929ffffab528a4548ded6
Prabudh tyagi <prabudhtyagi@gmail.com>
2026-08-04 22:52:27 +0530

    learning

--- Notes/typeCastingAndFunction.py
+++ Notes/typeCastingAndFunction.py
@@ -1,2 +1,14 @@
+#Type casting is a way to convert one data type into another data type. In python, we can use the following functions to perform type casting.
+
 a = 55
 print("Type of a",type(a))
+
+b = 5.5
+print("Type of b",type(b))
+
+c = "Hello"
+print("Type of c",type(c))
+
+print(str(a))
+print(int(a))
+print(float(a))
