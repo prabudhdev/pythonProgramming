e1e037e41681e27e6dd31a15117bb9a24b82c1e5
Prabudh tyagi <prabudhtyagi@gmail.com>
2026-08-09 17:30:25 +0530

    lerning

--- 04_string.py
+++ 04_string.py
@@ -4,4 +4,9 @@
 
 name = "Python"
 print(name[0:3])  #1st index included and 3rd  index not included.
-print(name[3:5])
+print(name[3:5]) 
+
+print(name[:4]) # if index  is not mentioned thn it will start from 0 index.
+
+print(name[4:]) # if end index is not mentioned then it will go till the end if the string.
+
