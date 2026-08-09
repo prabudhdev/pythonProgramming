563660f15e9196b199306aaa88e1b27efd364ef6
Prabudh tyagi <prabudhtyagi@gmail.com>
2026-08-10 01:54:06 +0530

    learning escape character

--- 05_strFunction.py
+++ 05_strFunction.py
@@ -1,11 +1,11 @@
-name = "python"
+name = "   python   "
 
 print(len(name))
 print(name.capitalize())
 print(name.upper())
 print(name.lower())
 print(name.title())
-print(name.find("programming"))
+print(name.find("python"))
 print(name.count("python"))
 print(name.strip())
 print(name.isalpha())
---
+++ 06_escapeSequence.py
@@ -0,0 +1,8 @@
+a = "python is a programming language.\nIt is a high level language"
+print(a)
+
+b = "python is a programming language.\tIt is a high level language"
+print(b)  
+
+c = "\"python\" is a programming language.It is a high level language"
+print(c)
