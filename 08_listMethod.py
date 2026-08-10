d52b92a28f62a7698cd80b7074019584f6f5dd1b
Prabudh tyagi <prabudhtyagi@gmail.com>
2026-08-11 01:43:53 +0530

    list method

---
+++ 08_listMethod.py
@@ -0,0 +1,11 @@
+ai = ["java","Python",56,44.66,False,"C#"]
+
+ai.append("JavaScript")
+
+print(ai)  
+
+lst1 = [1,4,5,77,88,34,5,675,44,0,6,433,3,2,111,678,89,9]
+lst1.sort()
+lst1.pop(6)
+lst1.reverse()
+print(lst1)
