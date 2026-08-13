104781d4ed285dbfed52116b01b7016a2ccaaf74
Prabudh tyagi <prabudhtyagi@gmail.com>
2026-08-13 22:49:20 +0530

    learning and practice quwation

--- 09_tuples.py
+++ 09_tuples.py
@@ -1,6 +1,11 @@
 #Tuples are immutable data types .
-#Tuple can`t be chnge
+#Tuple can`t be change
 
-a = (1,2,55.6,"harsh","rohan",False)
+a = (1,2,2,55.6,"harsh","Rohan",False)
+
+
+print(a.count(2))
 print(type(a))
-print(a)
+print(a) 
+print(a.index(False))
+print(len(a))
